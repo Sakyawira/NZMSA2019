473146cdc7cd4a089a1fae06df68a9bcda09502d
Sakyawira <sakyawira@gmail.com>
2019-07-25 20:41:26 +1200

    Databases

---
+++ DATABASE/schoolSIMS/schoolSIMS/Model/schoolSIMSContext.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace schoolSIMS.Model
+{
+    public partial class schoolSIMSContext : DbContext
+    {
+        public schoolSIMSContext()
+        {
+        }
+
+        public schoolSIMSContext(DbContextOptions<schoolSIMSContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Students> Students { get; set; }
+        public virtual DbSet<Transcription> Transcription { get; set; }
+        public virtual DbSet<Video> Video { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=tcp:nzmsasakyawira.database.windows.net,1433;Initial Catalog=scriber;Persist Security Info=False;User ID=sakyawira;Password=__Naruto55__;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<Students>(entity =>
+            {
+                entity.Property(e => e.Email).IsUnicode(false);
+
+                entity.Property(e => e.FirstName).IsUnicode(false);
+
+                entity.Property(e => e.LastName).IsUnicode(false);
+
+                entity.Property(e => e.PhoneNumber).IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Transcription>(entity =>
+            {
+                entity.Property(e => e.Phrase).IsUnicode(false);
+
+                entity.HasOne(d => d.Video)
+                    .WithMany(p => p.Transcription)
+                    .HasForeignKey(d => d.VideoId)
+                    .OnDelete(DeleteBehavior.Cascade)
+                    .HasConstraintName("VideoId");
+            });
+
+            modelBuilder.Entity<Video>(entity =>
+            {
+                entity.Property(e => e.ThumbnailUrl).IsUnicode(false);
+
+                entity.Property(e => e.VideoTitle).IsUnicode(false);
+
+                entity.Property(e => e.WebUrl).IsUnicode(false);
+            });
+        }
+    }
+}
